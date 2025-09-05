7841dba8f65bc23654d845d00b9102959615fbe1
fperez15 <francis.perez1590@gmail.com>
2025-09-04 19:58:10 -0500

    Evitando duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,18 +1,27 @@
-import { useState } from "react"
-import type { OrderItem, MenuItem } from '../types';
-
+import { useState } from "react";
+import type { OrderItem, MenuItem } from "../types";
 
 export default function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([]);
 
-    const [order, setOrder] = useState<OrderItem[]>([]);
-
-    const addItem = (item : MenuItem) => {
-        console.log(item);
+  // que elementos estan duplicados para asi incrementar su cantidad en quantity
+  const addItem = (item: MenuItem) => {
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExist) {
+      const updateOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updateOrder);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
     }
-    
-    return {
+  };
+  console.log(order)
 
-        addItem
-
-    }
+  return {
+    addItem,
+  };
 }
